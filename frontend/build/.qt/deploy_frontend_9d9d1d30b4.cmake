e764391d0b604060220a06ced1bee5dc1541af37
tnasanen24 <tommy.nasanen@gmail.com>
2026-01-12 19:22:01 +0200

    Revert "Transaction model added"

--- frontend/build/.qt/deploy_frontend_9d9d1d30b4.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/leipp/Documents/Tietotekniikka/ohjelmistokehityksen_sovellusprojekti/group_1/frontend/build/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/frontend-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/leipp/Documents/Tietotekniikka/ohjelmistokehityksen_sovellusprojekti/group_1/frontend/build/frontend.exe"
-    GENERATE_QT_CONF
-)
