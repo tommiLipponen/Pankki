50b352dece3ee8194931d0c8b8a76f043ce851dd
Tommy Näsänen <tommy.nasanen@gmail.com>
2026-01-19 18:37:18 +0200

    Transactions routes

--- backend/src/routes/transactionRoutes.js
+++ backend/src/routes/transactionRoutes.js
@@ -5,7 +5,6 @@ const express = require('express');
 const router = express.Router();
 const transactionController = require('../controllers/transactionController');
 
-
 /**
  * @swagger
  * /api/transactions/{accountId}:
