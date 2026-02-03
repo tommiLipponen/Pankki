e54ba0175e76d10f8508ffc16ef986cc276d5fa5
iisametsola <metsola.iisa@gmail.com>
2026-02-03 17:57:28 +0200

    testi

--- frontend/build/qtcsettings.cmake
+++ frontend/build/qtcsettings.cmake
@@ -1,2 +1,11 @@
 # This file is managed by Qt Creator, do not edit!
 
+set("CMAKE_C_COMPILER" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_COMPILER" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "" CACHE "PATH" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Ohjelmistoprojekti/group_1/frontend/build/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "C:/Qt/MaintenanceTool.exe" CACHE "FILEPATH" "" FORCE)
