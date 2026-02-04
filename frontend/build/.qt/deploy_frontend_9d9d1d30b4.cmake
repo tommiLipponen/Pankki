30cc152a70d2da19856bd659a45fb816656e5c24
Topi Pietilänaho <topi.pietilanaho99@gmail.com>
2026-02-04 18:34:20 +0200

    balancepage ready

--- frontend/build/.qt/deploy_frontend_9d9d1d30b4.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/GitHub/Ohjelmistoprojekti/group_1/frontend/build/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/frontend-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "D:/GitHub/Ohjelmistoprojekti/group_1/frontend/build/frontend.exe"
-    GENERATE_QT_CONF
-)
